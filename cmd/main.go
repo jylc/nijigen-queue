74d636fd7766df7c655ee871021839b840d7c329
王真 <1046060018@qq.com>
2021-07-15 11:29:37 +0800

    fixed use nijigen command bug

--- cmd/main.go
+++ cmd/main.go
@@ -9,54 +9,49 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const (
-	encodedLogo = "ICBfICAgXyBfX18gICAgXyBfX18gX19fXyBfX19fXyBfICAgXyAKIHwgXCB8IHxfIF98ICB8IHxfIF8vIF9fX3wgX19fX3wgXCB8IHwKIHwgIFx8IHx8IHxfICB8IHx8IHwgfCAgX3wgIF98IHwgIFx8IHwKIHwgfFwgIHx8IHwgfF98IHx8IHwgfF98IHwgfF9fX3wgfFwgIHwKIHxffCBcX3xfX19cX19fL3xfX19cX19fX3xfX19fX3xffCBcX3wKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICA="
-)
-
 var (
 	envv = ""
 
 	rootCmd = &cobra.Command{
-		Use:   "nijigen-queue",
+		Use:   "nijigen",
 		Short: "nijigen queue is a nijigen queue",
-		Run:   run,
 	}
 
 	runCmd = &cobra.Command{
 		Use:     "run",
-		Short:   "run the nijigen queue",
-		Long:    "run the nijigen queue",
+		Short:   "Run the nijigen queue",
+		Long:    "Run the nijigen queue",
 		Run:     run,
 		Example: "nijigen run --env dev",
 	}
 )
 
 func main() {
-	initFlag()
+	initCmd()
 
 	if err := rootCmd.Execute(); err != nil {
 		logger.Fatal([]byte(err.Error()))
 	}
 }
 
-func initFlag() {
-	logo, _ := base64.StdEncoding.DecodeString(encodedLogo)
-	rootCmd.SetHelpTemplate(string(logo) + "\n\n" + rootCmd.HelpTemplate())
+func initCmd() {
+	rootCmd.SetHelpTemplate(logo() + "\n\n" + rootCmd.HelpTemplate())
 	rootCmd.PersistentFlags().StringVarP(&envv, "env", "", env.Dev, "to decide program run with which env")
 	rootCmd.AddCommand(runCmd)
-
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
 }
 
 func run(cmd *cobra.Command, args []string) {
-	printLogo()
+	fmt.Println(logo())
 
 	env.Set(envv)
 
 	// TODO
 }
 
-func printLogo() {
-	logo, _ := base64.StdEncoding.DecodeString(encodedLogo)
-	fmt.Println(string(logo))
+const encodedLogo = "ICBfICAgXyBfX18gICAgXyBfX18gX19fXyBfX19fXyBfICAgXyAKIHwgXCB8IHxfIF98ICB8IHxfIF8vIF9fX3wgX19fX3wgXCB8IHwKIHwgIFx8IHx8IHxfICB8IHx8IHwgfCAgX3wgIF98IHwgIFx8IHwKIHwgfFwgIHx8IHwgfF98IHx8IHwgfF98IHwgfF9fX3wgfFwgIHwKIHxffCBcX3xfX19cX19fL3xfX19cX19fX3xfX19fX3xffCBcX3wKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICA="
+
+func logo() string {
+	res, _ := base64.StdEncoding.DecodeString(encodedLogo)
+	return string(res)
 }
